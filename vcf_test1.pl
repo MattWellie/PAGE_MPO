db0605bd87922bed98a549326eb2d0004d69ca4d
Matt Welland <mattwellie@gmail.com>
2016-04-11 10:51:23 +0100

    Added element access examples

--- vcf_test1.pl
+++ vcf_test1.pl
@@ -5,8 +5,19 @@ use DDP;
 my $vcf = Vcf->new(file=>'PP0001.vcf.gz');
 $vcf->parse_header();
 
-while (my $x=$vcf->next_data_hash()) 
-    {
-	p($x);
-	$this = <STDIN>;
-    }
+$line = $vcf->next_data_hash();
+
+my %vcf_hash = %{$line}; # This works
+p(%vcf_hash);
+
+my $vcf_pos = $line->{"POS"};
+print "POS = $vcf_pos \n";
+
+my $vcf_ref = $line->{"REF"};
+print "REF = $vcf_ref \n";
+
+my $vcf_2deep = $line->{"INFO"}{"AC"};
+print "AC = $vcf_2deep \n";
+
+my $vcf_3deep = $line->{"gtypes"}{"PP0001"}{"AD"};
+print "genotypes-sample-AD = $vcf_3deep \n";
